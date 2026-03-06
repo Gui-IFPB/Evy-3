13797a615acf57741a9fb672f82de495a64ee903
ifpb <ifpb@DSK14LB06-JP>
2026-03-06 09:27:28 -0300

    ARG Lirio Vermelho

---
+++ script.js
@@ -0,0 +1,21 @@
+function verificar(respostaCorreta,proximaPagina){
+
+let resposta=document
+.getElementById("resposta")
+.value
+.toLowerCase()
+.trim()
+
+if(resposta===respostaCorreta){
+
+window.location.href=proximaPagina
+
+}
+
+else{
+
+alert("Código incorreto.")
+
+}
+
+}
